7d52cbb52d7c6985effb97fa9dd90796a0f276a4
Sylwia Üçüncü <sylwia_uc@yahoo.com>
2016-11-21 22:54:55 +0100

    (fix) backstack controle

--- app/src/main/java/com/hfad/lookafter/TopFragment.java
+++ app/src/main/java/com/hfad/lookafter/TopFragment.java
@@ -3,13 +3,15 @@ package com.hfad.lookafter;
 import android.app.Fragment;
 import android.content.Intent;
 import android.database.Cursor;
-import android.database.sqlite.SQLiteException;
 import android.os.Bundle;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 import com.hfad.lookafter.activities.ContentActivity;
 
@@ -19,40 +21,26 @@ public class TopFragment extends Fragment {
     private Cursor favouriteCursor;
     private ConnectionManager connectionManager = new ConnectionManager();
     private RecyclerView bookRecycler;
-    ImagesAdapter imagesAdapter;
+    private int[] bookCovers;
+    private ImagesAdapter imagesAdapter;
 
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
         bookRecycler = (RecyclerView) inflater.inflate(R.layout.fragment_top, container, false);
+
         connectionManager.setDatabaseContext(inflater.getContext());
+        updateFavouriteList();
 
-        try {
-            favouriteCursor = connectionManager.getFavouriteBooks();
+        GridLayoutManager layoutManager = new GridLayoutManager(getActivity(), 2);
+        bookRecycler.setLayoutManager(layoutManager);
 
-            int[] bookCovers = new int[favouriteCursor.getCount()];
-            for (int i = 0; i < favouriteCursor.getCount(); i++) {
-                bookCovers[i] = getCoverId();
-            }
+//        if (bookCovers.length == 0) {
+//            TextView notice = new TextView(getActivity());
+//            notice.setText(R.string.empty_library);
+//            View.inflate().addView(notice);
+//        }
 
-            imagesAdapter = new ImagesAdapter(bookCovers);
-            bookRecycler.setAdapter(imagesAdapter);
-
-            GridLayoutManager layoutManager = new GridLayoutManager(getActivity(), 3);
-            bookRecycler.setLayoutManager(layoutManager);
-
-            imagesAdapter.setListener(new ImagesAdapter.Listener() {
-                @Override
-                public void onClick(int position) {
-                    long id = getItemId(position);
-                    Intent intent = new Intent(getActivity(), ContentActivity.class);
-                    intent.putExtra(ContentActivity.EXTRA_BOOKN0, (int) id);
-                    getActivity().startActivity(intent);
-                }
-            });
-        } catch (SQLiteException ex) {
-            ex.printStackTrace();
-
-        }
         return bookRecycler;
     }
 
@@ -61,9 +49,46 @@ public class TopFragment extends Fragment {
         super.onResume();
 
         if (imagesAdapter != null) {
-            // tu powinnac pobrac aktualna liste ksiazek - nowe zapytanie do bazy danych - i zaktualizowac liste krzazek w adapterze
+            updateFavouriteList();
             imagesAdapter.notifyDataSetChanged();
         }
+    }
+
+    private void updateFavouriteList() {
+        favouriteCursor = connectionManager.getFavouriteBooks();
+        bookCovers = generateCoversArray();
+        if (bookCovers.length == 0) showNotice();
+        setAdapter();
+    }
+
+    private int[] generateCoversArray() {
+        bookCovers = new int[favouriteCursor.getCount()];
+        for (int i = 0; i < favouriteCursor.getCount(); i++) {
+            bookCovers[i] = getCoverId();
+        }
+        return bookCovers;
+    }
+
+    private void showNotice(){
+        LinearLayout linearLayout = new LinearLayout(getActivity());
+        LayoutInflater inflater = (LayoutInflater)getActivity().getSystemService(getActivity().LAYOUT_INFLATER_SERVICE);
+        TextView notice = new TextView(getActivity());
+        notice.setText(R.string.empty_library);
+        linearLayout.addView(notice);
+    }
+
+    private void setAdapter() {
+        imagesAdapter = new ImagesAdapter(bookCovers);
+        bookRecycler.setAdapter(imagesAdapter);
+        imagesAdapter.setListener(new ImagesAdapter.Listener() {
+            @Override
+            public void onClick(int position) {
+                long id = getItemId(position);
+                Intent intent = new Intent(getActivity(), ContentActivity.class);
+                intent.putExtra(ContentActivity.EXTRA_BOOKN0, (int) id);
+                getActivity().startActivity(intent);
+            }
+        });
     }
 
     private int getCoverId() {
--- app/src/main/java/com/hfad/lookafter/activities/MainActivity.java
+++ app/src/main/java/com/hfad/lookafter/activities/MainActivity.java
@@ -6,6 +6,7 @@ import android.app.FragmentTransaction;
 import android.content.Intent;
 import android.content.res.Configuration;
 import android.os.Bundle;
+import android.support.design.widget.Snackbar;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.view.Menu;
@@ -15,35 +16,33 @@ import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
-import com.hfad.lookafter.Book;
 import com.hfad.lookafter.BooksListFragment;
 import com.hfad.lookafter.R;
 import com.hfad.lookafter.TopFragment;
-import com.hfad.lookafter.activities.NotificationActivity;
-
-import java.io.InputStreamReader;
-import java.io.Reader;
-import java.io.UnsupportedEncodingException;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
 public class MainActivity extends Activity {
+
     private String[] options;
     private ActionBarDrawerToggle actionBarDrawerToggle;
+    private boolean isBackFirstPressed = true;
 
-    @BindView(R.id.drawer) ListView drawerList;
-    @BindView(R.id.drawer_layout) DrawerLayout drawerLayout;
+    @BindView(R.id.drawer)
+    ListView drawerList;
+    @BindView(R.id.drawer_layout)
+    DrawerLayout drawerLayout;
 
     private class DrawerItemClickListener implements ListView.OnItemClickListener {
 
         @Override
-        public void onItemClick(AdapterView<?> parent, View view, int position, long id){
+        public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
             selectItem(position);
         }
-    };
+    }
+
+    ;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -54,7 +53,7 @@ public class MainActivity extends Activity {
         options = getResources().getStringArray(R.array.options);
         drawerList.setAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_list_item_activated_1, options));
         drawerList.setOnItemClickListener(new DrawerItemClickListener());
-        if (savedInstanceState == null){
+        if (savedInstanceState == null) {
             selectItem(0);
         }
         actionBarDrawerToggle = new ActionBarDrawerToggle(this, drawerLayout, R.string.open_drawer, R.string.close_drawer);
@@ -65,29 +64,29 @@ public class MainActivity extends Activity {
     }
 
     @Override
-    protected void onPostCreate(Bundle savedInstanceState){
+    protected void onPostCreate(Bundle savedInstanceState) {
         super.onPostCreate(savedInstanceState);
         actionBarDrawerToggle.syncState();
     }
 
     @Override
-    public void onConfigurationChanged(Configuration newConfig){
+    public void onConfigurationChanged(Configuration newConfig) {
         super.onConfigurationChanged(newConfig);
         actionBarDrawerToggle.onConfigurationChanged(newConfig);
     }
 
     @Override
-    public boolean onCreateOptionsMenu (Menu menu){
+    public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return super.onCreateOptionsMenu(menu);
     }
 
     @Override
-    public boolean onOptionsItemSelected(MenuItem item){
-        if(actionBarDrawerToggle.onOptionsItemSelected(item)){
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (actionBarDrawerToggle.onOptionsItemSelected(item)) {
             return true;
         }
-        switch (item.getItemId()){
+        switch (item.getItemId()) {
             case R.id.action_create_notification:
                 Intent intent = new Intent(this, NotificationActivity.class);
                 startActivity(intent);
@@ -100,33 +99,62 @@ public class MainActivity extends Activity {
         }
     }
 
-    private void selectItem(int position){
+    private void selectItem(int position) {
         Fragment fragment;
         switch (position) {
             case 1:
                 fragment = new BooksListFragment();
+                replaceFragment(fragment);
                 break;
             default:
                 fragment = new TopFragment();
+                if (getCurrentFragment() instanceof TopFragment) break;
+
+                FragmentTransaction ft = getFragmentManager()
+                        .beginTransaction()
+                        .add(R.id.fragment_container, fragment);
+                ft.commit();
         }
 
-        FragmentTransaction ft = getFragmentManager()
-                .beginTransaction().
-                 replace(R.id.fragment_container, fragment)
-                .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE);
-        ft.commit();
         setActionBarTitle(position);
         drawerLayout.closeDrawer(drawerList);
     }
 
-    private void setActionBarTitle(int position){
+    private void replaceFragment(Fragment fragment) {
+        FragmentTransaction ft = getFragmentManager()
+                .beginTransaction().
+                        replace(R.id.fragment_container, fragment)
+                .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE);
+        ft.addToBackStack(null);
+        ft.commit();
+    }
+
+    private void setActionBarTitle(int position) {
         String title;
-        if(position == 0){
+        if (position == 0) {
             title = getResources().getString(R.string.app_name);
-        } else{
+        } else {
             title = options[position];
         }
         getActionBar().setTitle(title);
     }
 
+    private Fragment getCurrentFragment() {
+        Fragment currentFragment = this.getFragmentManager().findFragmentById(R.id.fragment_container);
+        return currentFragment;
+    }
+
+    public void onBackPressed() {
+        if (getCurrentFragment() instanceof TopFragment) {
+            if (isBackFirstPressed) {
+                isBackFirstPressed = false;
+                Snackbar.make(findViewById(R.id.drawer), R.string.press_to_exit, Snackbar.LENGTH_LONG)
+                        .show();
+            } else {
+                super.onBackPressed();
+            }
+        } else {
+            super.onBackPressed();
+        }
+    }
 }
